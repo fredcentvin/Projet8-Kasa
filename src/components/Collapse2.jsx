1c34e2ed7fcb9fd91133c0c10c18ae6c6d84f872
fred centvin <fbplayback1@gmail.com>
2024-08-27 12:14:06 +0200

    Ajustements responsive et animations

--- src/components/Collapse2.jsx
+++ src/components/Collapse2.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react"
 import '../style/collapse.css'
 import arrow_down from '../assets/images/arrow_down.png'
-import arrow_up from '../assets/images/arrow_up.png'
+
 
 export default function Collapse ({title,content}) {
     const [open,setOpen]= useState(false);
+    const rotation= open? 0: 180; 
+    const translation= open? 0: 20;
+    
     return (
         <div className="containerCollapses">
-            <div className ="collapseTitle" onClick={() => setOpen(!open)}>{title}  <img src={open ? arrow_down:arrow_up} alt="Arrow" /></div>
-            {open && <div className="collapseContent">{content} </div>}
+            <div className ="collapseTitle" onClick={() => setOpen(!open)}>{title}  <img src={arrow_down} alt="Arrow" style={{transform:`rotate(${rotation}deg)`}} /></div>
+            {open && (<div className="collapseContent" style={{transform:`translateY(${translation}px)`,transition:`transform 0.7s ease`}}>{content}</div>)}
+            
         </div>
     )
 }
+
