01832135a18b5b1b2377a768bff5cf9f93ab70d8
fred centvin <fbplayback1@gmail.com>
2024-08-28 14:45:46 +0200

    modifs css, ajout animations collapse

--- src/components/Collapse2.jsx
+++ src/components/Collapse2.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import '../style/collapse.css'
 import arrow_down from '../assets/images/arrow_down.png'
 
 
 export default function Collapse ({title,content}) {
     const [open,setOpen]= useState(false);
+    const [animationClass, setAnimationClass] = useState('closed');
     const rotation= open? 0: 180; 
-    const translation= open? 0: 20;
+    useEffect(() => {
+        if (open) {
+            setAnimationClass('open');
+        } else {
+            setAnimationClass('closed');
+        }
+    }, [open]);
     
     return (
         <div className="containerCollapses">
             <div className ="collapseTitle" onClick={() => setOpen(!open)}>{title}  <img src={arrow_down} alt="Arrow" style={{transform:`rotate(${rotation}deg)`}} /></div>
-            {open && (<div className="collapseContent" style={{transform:`translateY(${translation}px)`,transition:`transform 0.7s ease`}}>{content}</div>)}
-            
+            {open && (<div className={`collapseContent ${animationClass}`}> {content}</div>)}
         </div>
     )
 }
--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -1,12 +1,12 @@
 import Bannerinfos from './Bannerinfos'
 import Collapse2 from '../components/Collapse2'
 import '../style/infos.css'
+
 const infos = [
    {title:"Fiabilité",content:"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."},
    {title:"Respect",content:"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."},
    {title:"Service",content:"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."},
    {title:"Sécurité",content:"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."},
-
 ]
 
 function Apropos() {
@@ -14,7 +14,7 @@ function Apropos() {
       <section>
          <Bannerinfos />
          <div className='gridCollapseInfos'>
-            {infos.map(info => <Collapse2 title={info.title} content={info.content} />)}
+         {infos.map(info => <Collapse2 title={info.title} content={info.content} />)}
          </div>
       </section>
    )
